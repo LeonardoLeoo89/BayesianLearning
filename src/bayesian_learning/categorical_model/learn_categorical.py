d0ddf2b7f0cac4c951b9f59d7b161c895949ca9b
M4322uni <piscitello.2130445@studenti.uniroma1.it>
2026-07-25 14:57:42 +0200

    refactoring
    Co-authored-by: leopr <leonardo.premutico@gmail.com>

---
+++ src/bayesian_learning/categorical_model/learn_categorical.py
@@ -0,0 +1,56 @@
+import pyagrum as gum
+from enum import Enum, auto
+
+class StructureAlgorithm(Enum):
+    HILL_CLIMBING = auto()
+    K2 = auto()
+    GENETIC_K2 = auto()
+    STRUCTURAL_EM = auto()
+    DAGMA = auto()
+    DAG_GNN = auto()
+    GRAN_DAG = auto()
+    PC = auto()
+    FCI = auto()
+    RFCI = auto()
+
+class ParameterAlgorithm(Enum):
+    MLE = auto()
+    EM = auto()
+    BAYESIAN_DIRICHLET_PRIORS = auto()
+    ROBUST_BAYESIAN_ESTIMATE = auto()
+    SHRINKAGE_EXIMATOR = auto()
+    UNSPECIFIED = auto()
+
+def learn(location: str, structure_algo: StructureAlgorithm,
+    parameter_algo: ParameterAlgorithm = ParameterAlgorithm.UNSPECIFIED) -> gum.BayesNet:
+    return gum.BayesNet()
+
+def learn_structure(location: str, algo: StructureAlgorithm) -> gum.BayesNet:
+    learner: gum.BNLearner = gum.BNLearner(location)
+
+    match algo:
+        case StructureAlgorithm.HILL_CLIMBING:
+            pass
+        case StructureAlgorithm.K2:
+            pass
+        case StructureAlgorithm.GENETIC_K2:
+            pass
+        case StructureAlgorithm.STRUCTURAL_EM:
+            pass
+        case StructureAlgorithm.DAGMA:
+            pass
+        case StructureAlgorithm.DAG_GNN:
+            pass
+        case StructureAlgorithm.GRAN_DAG:
+            pass
+        case StructureAlgorithm.PC:
+            pass
+        case StructureAlgorithm.FCI:
+            pass
+        case StructureAlgorithm.RFCI:
+            pass
+
+
+    return gum.BayesNet()
+
+def learn_parameters(data, net: gum.BayesNet, algo: ParameterAlgorithm):
