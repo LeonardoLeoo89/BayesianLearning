9a3b1e6a5ffb5981e08ca843e842be0716e55f14
M4322uni <piscitello.2130445@studenti.uniroma1.it>
2026-07-26 00:09:39 +0200

    started work on translation

--- src/bayesian_learning/categorical_model/categorical_translator/tetrad2agrum.py
+++ src/bayesian_learning/categorical_model/categorical_translator/tetrad2agrum.py
@@ -1,6 +1,18 @@
 import pyagrum as gum
 from pytetrad.tools.TetradSearch import TetradSearch
 from typing import Any
+from jpype import JClass
+
+DiscreteVariable: JClass = JClass("edu.cmu.tetrad.data.DiscreteVariable")
 
 def translate(tetrad: Any) -> gum.BayesNet:
-    pass
+    out: gum.BayesNet = gum.BayesNet()
+
+    for node in tetrad.getNodes():
+        pass
+    for edge in tetrad.getEdges():
+        node1 = edge.getNode1()
+        node2 = edge.getNode2()
+        if str(edge.getEndpoint1()) == "TAIL":
+            pass
+    return out
