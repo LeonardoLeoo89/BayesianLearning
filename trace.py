f3e408da789a711b7212fe164c987bd3c1e2c622
M4322uni <piscitello.2130445@studenti.uniroma1.it>
2026-08-05 18:43:13 +0200

    remove temps

--- trace.py
+++
@@ -1,7 +0,0 @@
-import traceback
-from bayesian_learning.categorical_model.learn_categorical import learn, StructureAlgorithm, ParameterAlgorithm
-
-try:
-    learn('tests/synthetic_data/tsunami_subset_500.csv', StructureAlgorithm.GENETIC_K2, ParameterAlgorithm.MLE)
-except Exception as e:
-    traceback.print_exc()
